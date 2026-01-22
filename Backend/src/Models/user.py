1cdab0770390d237a6690be2e2c9901c5ef4122b
alisar <alisar.jaber2007@gmail.com>
2026-01-22 13:51:07 +0200

    Add database connection and user model

---
+++ Backend/src/Models/user.py
@@ -0,0 +1,12 @@
+from typing import Optional
+from sqlmodel import SQLModel, Field
+
+class User(SQLModel, table=True):
+    __tablename__ = "users"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    first_name: str
+    last_name: str
+    email: str = Field(index=True, unique=True)
+    hashed_password: str
+    is_admin: bool = Field(default=False)
