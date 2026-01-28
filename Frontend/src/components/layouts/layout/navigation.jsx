687e8d3930ceea0f93e2da569824ad96d645ebf4
Lana <lana.h0807@gmail.com>
2026-01-28 11:17:56 +0200

    style

--- Frontend/src/components/layouts/layout/navigation.jsx
+++ Frontend/src/components/layouts/layout/navigation.jsx
@@ -7,6 +7,7 @@ import { useNavigate } from "react-router-dom";
 const Navigation = () => {
 
     const navigate = useNavigate();
+    const one = 1
 
     const handleLogout = async () => {
         await axios.post(
